0e04553effcbca78e8017e3f8c912e62ecdc81e7
Xpektra7 <154679547+Xpektra7@users.noreply.github.com>
2025-04-04 21:55:13 +0100

    Update Tip.jsx

--- src/Tip.jsx
+++ src/Tip.jsx
@@ -3,7 +3,7 @@ export default function Tip() {
     const [hidden,setHidden] = useState(false);
   return (
     <div  className="tip">
-      <button onClick={() => setHidden(prev => !prev)} >Show Tip</button>
+      <button onClick={() => setHidden(prev => !prev)} >Toggle Tip</button>
       <div className={hidden? "" : "hidden"} >
         <h1>Tip</h1>
         <p>1. Click on the cards to hold them.</p>
